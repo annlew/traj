c0fc0d11867f3d5ddb30e5d07859bd09fc15635a
Anna Lewinschal <sr9@ac6-101.bullx>
2023-04-25 14:27:59 +0000

    change permissions

--- run_trajectories/linkPfiles.sh
+++ run_trajectories/linkPfiles.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-Pfile_dir=/proj/bolinc/users/x_micka/LAGRANTO_runs/out_ARTofMELT_20200417
+#Pfile_dir=/scratch/${USER}/LAGRANTO_runs/out_ARTofMELT_20200417
+Pfile_dir=/scratch/${USER}/testdata/ec.oper/20230418_00/cdf/
+
+echo ${Pfile_dir}
 
 ln -s ${Pfile_dir}/P* .
