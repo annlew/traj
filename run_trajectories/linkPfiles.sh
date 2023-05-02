8a547f9dfcce929eef0f7da4bc98d18557820a3f
Anna Lewinschal <sr9@ac6-101.bullx>
2023-05-02 14:18:59 +0000

    remove unused link files

--- run_trajectories/linkPfiles.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#Pfile_dir=/scratch/${USER}/LAGRANTO_runs/out_ARTofMELT_20200417
-Pfile_dir=/scratch/${USER}/testdata/ec.oper/20230418_00/cdf/
-
-echo ${Pfile_dir}
-
-ln -s ${Pfile_dir}/P* .
